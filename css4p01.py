bad9bc7e90b49f5082bfe79186cb4acf6539c5a2
JaydonKDurow <157606884+JaydonKDurow@users.noreply.github.com>
2024-02-04 21:10:45 +0200

    Update css4p01.py

--- css4p01.py
+++ css4p01.py
@@ -8,7 +8,8 @@ Created on Sun Feb  4 15:49:37 2024
 import pandas as pd
 
 '''Read in the csv file using the Rank column as the index and fix the spaces 
- in the column names'''
+ in the column names '''
+#Question 1 - done with variable explorer
 column_names = ["Rank", "Title", "Genre", "Description", "Director", "Actors",
                 "Year", "Runtime_Minutes", "Rating", "Votes", "Revenue_Millions", "Metascore"]
 df = pd.read_csv("movie_dataset.csv", index_col=0,
@@ -16,6 +17,7 @@ df = pd.read_csv("movie_dataset.csv", index_col=0,
 
 
 # Fill the NaN values in the revenue column with the mean value
+#Question 2
 rev = df["Revenue_Millions"].mean()
 df.fillna(rev, inplace=True)
 
@@ -28,23 +30,28 @@ df.drop_duplicates(inplace=True)
 
 # Filter data by year
 df2015 = df[df["Year"] == 2015]
-df2016 = df[df["Year"] == 2016]
+df2016 = df[df["Year"] == 2016] #Question 4 - read from variable explorer
 df2017 = df[df["Year"] == 2017]
 
 # Concatinat the three years
 df_2015_to_2017 = pd.concat([df2015, df2016, df2017], ignore_index=True)
 
+#Question 3
 avg_revenue_2015_to_2017 = df_2015_to_2017["Revenue_Millions"].mean()
 
 # Filter data for christopher nolan
+#Question 5 read from variable explorer
 df_Christopher_Nolan = df[df["Director"] == "Christopher Nolan"]
 
 # Filter data for rating greater than or equal to 8.0
+#Question 6 read from variable explorer
 df_rating8 = df[df["Rating"] >= 8.0]
 
+#Question 7
 df_Christopher_Nolan_avg_rating = df_Christopher_Nolan["Rating"].mean()
 
 # Filter years
+#Question 8
 df2006 = df[df["Year"] == 2006]
 df2007 = df[df["Year"] == 2007]
 df2008 = df[df["Year"] == 2008]
@@ -54,12 +61,14 @@ df2007_avg_rating = df2007["Rating"].mean()
 df2008_avg_rating = df2008["Rating"].mean()
 df2016_avg_rating = df2016["Rating"].mean()
 
+#Question 9
 percentage_increase_2006_to_2016 = ((297-44) / 44) * 100
 
 #Split actors column
 df[['Actor1', 'Actor2', 'Actor3', 'Actor4']] = df['Actors'].str.split(',', n=3, expand=True)
 
 #Check if actor is listed in any of the actor columns
+#Question 10
 '''Bradley Cooper, Mark Wahlberg, Chris Pratt, Matthew McConaughey'''
 actor = "Mark Wahlberg"
 
@@ -82,10 +91,12 @@ df_genre2.dropna(inplace=True)
 df_genre3.dropna(inplace=True)
 
 #Concatinate the 3 data frames and drop duplicates
+#Question 11
 df_genre_total = pd.concat([df_genre1, df_genre2, df_genre3], ignore_index=True)
 df_genre_total.drop_duplicates(inplace = True)
 
 #Data correlation
+#Question 12
 df_describe = df.describe()
 df.info()
 
